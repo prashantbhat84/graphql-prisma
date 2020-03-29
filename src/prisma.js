7cbbb1124906039b2c27c56cfa5de604091ffa0d
Prashant Bhat <prashantbhat84@gmail.com>
2020-03-29 14:27:41 +0530

    video 52
    mutation with prisma bindings

--- src/prisma.js
+++ src/prisma.js
@@ -7,10 +7,59 @@ const prisma = new Prisma({
 //prisma.qery  prisma,mutation prisma.sub  prisma.exists
 
 // prisma.query.users(null, "{id name posts {id title}}").then(data => {
-//   console.log(JSON.stringify(data,undefined,2));
+//   console.log(JSON.stringify(data, undefined, 2));
 // });
 // prisma.query
 //   .comments(null, "{id text author{id name} post{ id title}}")
 //   .then(data => {
 //     console.log(JSON.stringify(data, undefined, 4));
 //   });
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "101 graphql",
+//         body: "",
+//         published: false,
+//         author: {
+//           connect: {
+//             id: "ck8btkasj00fg070383qxke4v"
+//           }
+//         }
+//       }
+//     },
+//     "{id title body published}"
+//   )
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//     return prisma.query.users(null, "{id name posts {id title}}").then(data => {
+//       console.log(JSON.stringify(data, undefined, 2));
+//     });
+//   });
+// prisma.query
+//   .posts(null, "{id title body published author {id name email}}")
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 4));
+//     console.log(data.length);
+//   });
+prisma.mutation
+  .updatePost(
+    {
+      where: {
+        id: "ck8cs28wu003i070337n662qb"
+      },
+      data: {
+        body: "This is a new course",
+        published: true
+      }
+    },
+    "{id title body published}"
+  )
+  .then(data => {
+    console.log(data);
+    prisma.query
+      .posts(null, "{id title body published author{ id name email}}")
+      .then(data => {
+        console.log(JSON.stringify(data, undefined, 4));
+      });
+  });
