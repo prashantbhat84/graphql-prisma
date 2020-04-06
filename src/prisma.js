ceb5d9f1ff6faf7d2b9144827c3a05aa3c0478b0
Prashant Bhat <prashantbhat84@gmail.com>
2020-04-06 13:52:41 +0530

    prisma lock

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://localhost:4466"
+  endpoint: "http://localhost:4466",
+  secret: "AndrewMeadPrashantUdemy@1234567890",
 });
 export { prisma as default };
 //prisma.qery  prisma,mutation prisma.sub  prisma.exists
